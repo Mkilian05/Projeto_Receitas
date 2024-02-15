692a167fb9a14032f8a757e4490b1fe99e666ca4
mkilian05 <mkilian05@gmail.com>
2024-02-15 11:11:50 -0300

    Teste-002

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,38 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container-fluid mt-5">
+    <div class="container-fluid mt-5">
 
-    {{--  Imagem/Banner  --}}
-    <img src="..." class="img-fluid" alt="...">
+        {{--  Imagem/Banner  --}}
+        <img src="..." class="img-fluid" alt="...">
 
-    {{--  Grupo de Cards  --}}
-    <div class="card-group">
-        {{--  Primeiro Card  --}}
-        <div class="card text-center">
-            <img src="{{ asset("images/Receitas_Salgadas.png") }}" class="card-img-top" width="250" height="250" alt="Card Image 1">
-            <div class="card-body">
-                <h5 class="card-title"></h5>
-                <p class="card-text">Para visualizar <strong>SOMENTE</strong> Receitas Salgadas clique no botão abaixo!</p>
-                <a href="#" class="btn btn-outline-info btn-lg">Vamos lá!</a>
+        {{--  Grupo de Cards  --}}
+        <div class="card-group">
+            {{--  Primeiro Card  --}}
+            <div class="card text-center">
+                <img src="{{ asset('images/Receitas_Salgadas.png') }}" class="card-img-top" width="250" height="250"
+                    alt="Card Image 1">
+                <div class="card-body">
+                    <h5 class="card-title"></h5>
+                    <p class="card-text">Para visualizar <strong>SOMENTE</strong> Receitas Salgadas clique no botão abaixo!
+                    </p>
+                    <a href="#" class="btn btn-outline-info btn-lg">Vamos lá!</a>
+                </div>
+            </div>
+
+            {{--  Segundo Card  --}}
+            <div class="card text-center">
+                <img src="{{ asset('images/Receitas_Doces.png') }}" class="card-img-top" width="250" height="250"
+                    alt="Card Image 2">
+                <div class="card-body">
+                    <h5 class="card-title ">Receitas Doces</h5>
+                    <p class="card-text ">Para visualizar <strong>SOMENTE</strong> Receitas Doces clique no botão abaixo!
+                    </p>
+                    <a href="#" class="btn btn-outline-info btn-lg">Vamos lá!</a>
+                </div>
             </div>
         </div>
-
-        {{--  Segundo Card  --}}
-        <div class="card text-center">
-            <img src="{{ asset("images/Receitas_Doces.png") }}" class="card-img-top" width="250" height="250" alt="Card Image 2">
-            <div class="card-body">
-                <h5 class="card-title ">Receitas Doces</h5>
-                <p class="card-text ">Para visualizar <strong>SOMENTE</strong> Receitas Doces clique no botão abaixo!</p>
-                <a href="#" class="btn btn-outline-info btn-lg">Vamos lá!</a>
-            </div>
-        </div>
+        {{--  Fim Grupo de Cards  --}}
     </div>
-    {{--  Fim Grupo de Cards  --}}
-</div>
-</div>
+    </div>
 @endsection
