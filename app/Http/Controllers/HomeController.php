133dba555294d35e81bf3b4e00376b89bf936426
mkilian <mkilian05@gmail.com>
2024-06-19 09:04:07 -0300

    Config. Slug

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Receita;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('index');
+        $receitas = Receita::all();
+        return view('index',['receitas' => $receitas]);
     }
 }
